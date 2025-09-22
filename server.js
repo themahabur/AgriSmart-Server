5d20a45f84030f9a9fb6929559ce9970efa3dcfb
MAHIR-DEVES <mdmeherab657@gmail.com>
2025-09-22 22:00:46 +0600

    create folder structure

---
+++ server.js
@@ -0,0 +1,17 @@
+// server.js
+const express = require('express');
+const app = express();
+const PORT = 5000;
+
+// Middleware to parse JSON
+app.use(express.json());
+
+// Basic route
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});

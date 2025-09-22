85dc8eb21155d5aec431c4bd38830c7cb84c8cca
MAHIR-DEVES <mdmeherab657@gmail.com>
2025-09-22 22:17:18 +0600

    databse connect succesfully

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 // server.js
+const mongoose = require('mongoose');
 const express = require('express');
+const connectDB = require('./src/config/db');
 const app = express();
 const PORT = 5000;
+connectDB();
 
 // Middleware to parse JSON
 app.use(express.json());

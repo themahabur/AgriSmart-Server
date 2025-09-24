3e340f5992ef7b307c9f68f72dcc8134dfe53fba
MAHIR-DEVES <mdmeherab657@gmail.com>
2025-09-24 11:54:00 +0600

    response updated

--- src/controller/loginController.js
+++ src/controller/loginController.js
@@ -1,6 +1,6 @@
-const Users = require("../module/userModule");
-const bcrypt = require("bcryptjs");
-const { generateToken } = require("../utils/jwt");
+const Users = require('../module/userModule');
+const bcrypt = require('bcryptjs');
+const { generateToken } = require('../utils/jwt');
 
 exports.loginUser = async (req, res) => {
   try {
@@ -9,19 +9,19 @@ exports.loginUser = async (req, res) => {
     if (!email || !password) {
       return res
         .status(400)
-        .json({ status: false, message: "Email and password are required" });
+        .json({ status: false, message: 'Email and password are required' });
     }
 
     const user = await Users.findOne({ email });
     if (!user) {
-      return res.status(404).json({ status: false, message: "User not found" });
+      return res.status(404).json({ status: false, message: 'User not found' });
     }
 
     const isMatch = bcrypt.compareSync(password, user.password);
     if (!isMatch) {
       return res
         .status(401)
-        .json({ status: false, message: "Incorrect password" });
+        .json({ status: false, message: 'Incorrect password' });
     }
 
     const tokenPayload = { id: user._id, email: user.email };
@@ -29,11 +29,14 @@ exports.loginUser = async (req, res) => {
 
     res.status(200).json({
       status: true,
-      message: "Login successful",
+      message: [
+        { message: 'Login successful' },
+        { message: 'হালকা মুতে শুতে যাও লগইন হয়ে গেছে !😴' },
+      ],
       token,
     });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ status: false, message: "Server error" });
+    res.status(500).json({ status: false, message: 'Server error' });
   }
 };

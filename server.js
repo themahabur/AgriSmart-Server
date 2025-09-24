653abe9ae14375e0800164319ff2e8cd663f8863
MAHIR-DEVES <mdmeherab657@gmail.com>
2025-09-24 23:51:19 +0600

    setup cors

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const dotenv = require("dotenv");
-const userRouter = require("./src/router/userRouter");
-const connectDB = require("./src/config/db");
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const userRouter = require('./src/router/userRouter');
+const connectDB = require('./src/config/db');
 
 dotenv.config();
 
@@ -10,22 +11,23 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
+app.use(cors());
 app.use(express.json());
 
 // Routes
-app.use("/api/users", userRouter);
+app.use('/api/users', userRouter);
 
 // MongoDB connection
 connectDB();
 
-app.get("/", (req, res) => {
-  res.send("Agrismart server is running");
+app.get('/', (req, res) => {
+  res.send('Agrismart server is running');
 });
 
 // Export the Express app for Vercel
 module.exports = app;
 
 // Start server (only for local development)
-if (process.env.NODE_ENV !== "production") {
+if (process.env.NODE_ENV !== 'production') {
   app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 }

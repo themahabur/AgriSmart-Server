bdaccebf3d7e2082227bb2f134a4b52429a8a20c
ana-yet <anayethusenaj@gmail.com>
2025-10-15 12:37:34 +0600

    added delete all history route

--- src/router/aiHistoryRouter.js
+++ src/router/aiHistoryRouter.js
@@ -3,12 +3,14 @@ const express = require("express");
 const {
   createHistory,
   getAllAiHistory,
+  deleteAllHistory,
 } = require("../controller/aiHistoryController");
 
 const router = express.Router();
 
 // AI History Routes
-router.post("/", createHistory);
 router.get("/", getAllAiHistory);
+router.post("/", createHistory);
+router.delete("/", deleteAllHistory);
 
 module.exports = router;

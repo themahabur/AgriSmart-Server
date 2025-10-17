604aef1f5a1ca6c339e94d003c0d38f7c2268f86
ana-yet <anayethusenaj@gmail.com>
2025-10-17 17:25:30 +0600

    added get all task by email route

--- src/router/farmTaskRouter.js
+++ src/router/farmTaskRouter.js
@@ -1,10 +1,14 @@
 const express = require("express");
 
-const { createFarmTask } = require("../controller/farmTaskController");
+const {
+  createFarmTask,
+  getTasksByEmail,
+} = require("../controller/farmTaskController");
 
 const router = express.Router();
 
 // Route to create a new farm task
 router.post("/farm-tasks", createFarmTask);
+router.get("/farm-tasks/:email", getTasksByEmail);
 
 module.exports = router;

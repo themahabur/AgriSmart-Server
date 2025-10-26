761a1182ff69acac34f8728043e4c6947d57671e
ana-yet <anayethusenaj@gmail.com>
2025-10-26 11:27:19 +0600

    added authentication in farm router and task add middleware

--- src/router/farmRouter.js
+++ src/router/farmRouter.js
@@ -13,9 +13,26 @@ const router = express.Router();
 
 // Farms (all routes are protected)
 router.get("/farms/:email", listFarms);
-router.post("/farms", recordActivity("farm_create"), createFarm);
+router.post(
+  "/farms",
+  authenticateToken,
+  recordActivity("farm_create"),
+  createFarm
+);
 router.get("/farms/:id", getFarmById);
-router.put("/farms/:id", recordActivity("farm_update"), updateFarm);
-router.delete("/farms/:id", recordActivity("farm_delete"), deleteFarm);
+
+router.put(
+  "/farms/:id",
+  authenticateToken,
+  recordActivity("farm_update"),
+  updateFarm
+);
+
+router.delete(
+  "/farms/:id",
+  authenticateToken,
+  recordActivity("farm_delete"),
+  deleteFarm
+);
 
 module.exports = router;

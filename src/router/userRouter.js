d0646c8a52f29c1456131d65d41f621bdce89a63
ana-yet <anayethusenaj@gmail.com>
2025-10-30 15:17:22 +0600

    updated get user by email to get user by token. created a controller update user for updating current user

--- src/router/userRouter.js
+++ src/router/userRouter.js
@@ -10,6 +10,7 @@ const {
   searchUsers,
   getAllUsers,
   getUserById,
+  updateUser,
 } = require("../controller/userContoller");
 const { authenticateToken } = require("../middleware/authentication");
 
@@ -20,10 +21,13 @@ router.post("/login", loginUser); // login
 router.post("/google", googleRegister); // google registration
 
 // GET routes for user data
-// router.get("/me", authenticateToken, getCurrentUser); // Get current authenticated user's data
-router.get("/me/:email", getUserByEmail); // Get current authenticated user's data
+router.get("/me", authenticateToken, getCurrentUser); // Get current authenticated user's data
+// router.get("/me/:email", getUserByEmail); // Get current authenticated user's data
 router.get("/search", searchUsers); // Search users (public)
 router.get("/all", getAllUsers); // Get all users with pagination (protected)
 router.get("/:userId", getUserById); // Get user by ID (public profile)
+
+// Update user profile (only for the user themselves)
+router.patch("/me", authenticateToken, updateUser);
 
 module.exports = router;
